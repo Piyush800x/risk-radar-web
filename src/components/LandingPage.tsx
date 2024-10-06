f991dbd5f01973950e37202d05ace60794164206
shibammitra24 <shibammitra695@gmail.com>
2024-10-06 22:44:01 +0530

    Added view-details

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -7,6 +7,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
+
 import {
   Dialog,
   DialogContent,
@@ -83,7 +84,7 @@ export function LandingPage() {
               <DialogTrigger className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 w-36">
                 Add product
               </DialogTrigger>
-              <DialogContent>
+              <DialogContent className="">
                 <DialogHeader>
                   <DialogTitle>Add Products</DialogTitle>
                   <DialogDescription>
