88d21b288b43bfc33791eda972aeb201a508863d
shibammitra24 <shibammitra695@gmail.com>
2024-10-23 02:02:51 +0530

    NavBar update and Search bar integration

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import ProductCard from "@/components/ProductCard";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import { redirect } from "next/navigation";
-import { Input } from "@/components/ui/input";
-import { Search } from "lucide-react";
 
 export default function Dashboard() {
   const { isAuthenticated, isLoading } = useKindeBrowserClient();
@@ -16,19 +13,7 @@ export default function Dashboard() {
 
       {isAuthenticated ? (
         <div className="flex flex-col gap-4">
-          {/* <SideBar /> */}
-          {/* <div className="w-1/4 flex items-center">
-            <Search className="left-6 absolute size-5" />
-            <Input placeholder="Search by product name" className="px-9"/>
-          </div>
-          <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-4 w-full">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-          </div> */}
+          
         </div>
       ) : (
         redirect("/")
