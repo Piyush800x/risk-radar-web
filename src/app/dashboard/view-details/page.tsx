b93623c65e1f74c1028dd0dea0d3eea0c3a9c043
shibammitra24 <shibammitra695@gmail.com>
2024-10-30 23:13:52 +0530

    Small UI changes

--- src/app/dashboard/view-details/page.tsx
+++ src/app/dashboard/view-details/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import { ObjectId } from "mongodb";
 import { useState, useEffect } from "react";
@@ -83,59 +81,7 @@ export default function ViewDetails() {
       {cveResults.map((cve, index) => (
         <ViewDetailsComp cve={cve} index={index}/>
       ))}
-      {/* <div className="bg-[#FFBDBD] dark:bg-[#FF6565]/20 p-4 rounded-md flex flex-col justify-between mb-4">
-        <div className="flex justify-between items-start w-full">
-          <div>
-            <h1 className="text-lg font-semibold">CVE ID: CVE-2023-25280</h1>
-            <h1>
-              OS Command injection vulnerability in D-Link DIR820LA1_FW105B03
-              allows attackers to escalate privileges to root via a crafted
-              payload with the ping_addr parameter to ping.ccp.
-            </h1>
-          </div>
-          <div className="flex gap-1 items-center">
-            <Image
-              src={"/red_ellipse.svg"}
-              height={20}
-              width={20}
-              alt="red_ellipse"
-            />
-            <h1 className="font-semibold">Critical</h1>
-          </div>
-        </div>
-        <div className="flex w-full justify-end mt-8">
-          <Button className="bg-[#fafafa] text-[#000000] hover:bg-[#fafafa]/90">
-            Learn more
-          </Button>
-        </div>
-      </div>
-
-      <div className="bg-[#FFC986] dark:bg-[#DB8F00]/40 p-4 rounded-md flex flex-col justify-between">
-        <div className="flex justify-between items-start w-full">
-          <div>
-            <h1 className="text-lg font-semibold">CVE ID: CVE-2023-25280</h1>
-            <h1>
-              OS Command injection vulnerability in D-Link DIR820LA1_FW105B03
-              allows attackers to escalate privileges to root via a crafted
-              payload with the ping_addr parameter to ping.ccp.
-            </h1>
-          </div>
-          <div className="flex gap-1 items-center">
-            <Image
-              src={"/yellow_ellipse.svg"}
-              height={20}
-              width={20}
-              alt="yellow_ellipse"
-            />
-            <h1 className="font-semibold">High</h1>
-          </div>
-        </div>
-        <div className="flex w-full justify-end mt-8">
-          <Button className="bg-[#fafafa] text-[#000000] hover:bg-[#fafafa]/90">
-            Learn more
-          </Button>
-        </div>
-      </div> */}
+    
       </div>
     </>
   );
--- src/components/ViewDetailsComp.tsx
+++ src/components/ViewDetailsComp.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import ReactMarkdown from "react-markdown";
@@ -6,95 +6,92 @@ import rehypeRaw from "rehype-raw";
 import { useState } from "react";
 
 interface CVEData {
-    cveId: string;
-    aiSolution: string;
-    epssScore: string;
-    maxCvssBaseScore: string;
-    nvdVulnStatus: string;
+  cveId: string;
+  aiSolution: string;
+  epssScore: string;
+  maxCvssBaseScore: string;
+  nvdVulnStatus: string;
 }
 
 interface Props {
-    cve: CVEData;
-    index: number;
+  cve: CVEData;
+  index: number;
 }
 
-export default function ViewDetailsComp({cve, index}: Props) {
-    const [isExpanded, setIsExpanded] = useState<boolean>(false);
+export default function ViewDetailsComp({ cve, index }: Props) {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
-    const toggleExpand = () => {
-        setIsExpanded(!isExpanded);
-    };
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
 
-    return (
-        <div>
+  return (
+    <div>
+      <div
+        key={index}
+        className={`p-4 rounded-md flex flex-col justify-between mb-4 mx-3 ${
+          Number(cve.maxCvssBaseScore) >= 9
+            ? "bg-[#ffabab] dark:bg-[#FF6565]/20"
+            : "bg-[#FFC986] dark:bg-[#DB8F00]/30"
+        }`}
+      >
+        <div className="flex flex-col-reverse gap-2 sm:flex sm:flex-row sm:justify-between sm:items-start">
+          {/* CVE Id and AISummary */}
+          <div className="">
+            <h1 className="text-lg font-semibold">CVE ID: {cve.cveId}</h1>
             <div
-                key={index}
-                className={`p-4 rounded-md flex flex-col justify-between mb-4 mx-3 ${
-                Number(cve.maxCvssBaseScore) >= 9
-                    ? "bg-[#ffabab] dark:bg-[#FF6565]/20"
-                    : "bg-[#FFC986] dark:bg-[#DB8F00]/30"
-                }`}
+              className={`prose dark:invert flex flex-col w-full overflow-hidden transition-all duration-500 ${
+                isExpanded ? "max-h-full" : "max-h-48"
+              }`}
             >
-                <div className="flex flex-col-reverse gap-2 sm:flex sm:flex-row sm:justify-between sm:items-start">
-                {/* CVE Id and AISummary */}
-                <div className="">
-                    <h1 className="text-lg font-semibold">CVE ID: {cve.cveId}</h1>
-                    <div
-                    className={`prose dark:invert flex flex-col w-full overflow-hidden transition-all duration-500 ${
-                        isExpanded ? "max-h-max" : "max-h-48"
-                    }`}
-                    >
-                    <ReactMarkdown
-                        rehypePlugins={[rehypeRaw]}
-                        components={{
-                        a: ({ href, children }) => (
-                            <a
-                            href={href}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            >
-                            {children}
-                            </a>
-                        ),
-                        }}
-                        className={"overflow-hidden"}
-                    >
-                        {cve.aiSolution}
-                    </ReactMarkdown>
-                    </div>
-                </div>
-
-                {/* CVSS Score and indicator */}
-                <div className="flex gap-1 items-center">
-                    <h1 className="font-semibold">
-                    {Number(cve.maxCvssBaseScore)}
-                    </h1>
-                    <Image
-                    src={
-                        Number(cve.maxCvssBaseScore) >= 9
-                        ? "/red_ellipse.svg"
-                        : "/yellow_ellipse.svg"
-                    }
-                    height={20}
-                    width={20}
-                    alt={
-                        Number(cve.maxCvssBaseScore) >= 9
-                        ? "red_ellipse"
-                        : "yellow_ellipse"
-                    }
-                    />
-                    <h1 className="font-semibold">
-                    {Number(cve.maxCvssBaseScore) >= 9 ? "Critical" : "High"}
-                    </h1>
-                </div>
-                </div>
-
-                <div className="flex w-full justify-end mt-8">
-                    <Button className="bg-[#fafafa] text-[#000000] hover:bg-[#fafafa]/90" onClick={toggleExpand}>
-                        See full solution
-                    </Button>
-                </div>
+              <ReactMarkdown
+                rehypePlugins={[rehypeRaw]}
+                components={{
+                  a: ({ href, children }) => (
+                    <a href={href} target="_blank" rel="noopener noreferrer">
+                      {children}
+                    </a>
+                  ),
+                }}
+                className={"overflow-hidden"}
+              >
+                {cve.aiSolution}
+              </ReactMarkdown>
             </div>
+          </div>
+
+          {/* CVSS Score and indicator */}
+          <div className="flex gap-1 items-center">
+            <h1 className="font-semibold">{Number(cve.maxCvssBaseScore)}</h1>
+            <Image
+              src={
+                Number(cve.maxCvssBaseScore) >= 9
+                  ? "/red_ellipse.svg"
+                  : "/yellow_ellipse.svg"
+              }
+              height={20}
+              width={20}
+              alt={
+                Number(cve.maxCvssBaseScore) >= 9
+                  ? "red_ellipse"
+                  : "yellow_ellipse"
+              }
+            />
+            <h1 className="font-semibold">
+              {Number(cve.maxCvssBaseScore) >= 9 ? "Critical" : "High"}
+            </h1>
+          </div>
         </div>
-    )
+
+        <div className="flex w-full justify-end mt-8">
+          <Button
+            className="bg-[#fafafa] text-[#000000] hover:bg-[#fafafa]/90"
+            onClick={toggleExpand}
+          >
+            {isExpanded ? "Read Less" : "Read More"}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
