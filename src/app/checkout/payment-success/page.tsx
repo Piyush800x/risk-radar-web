93953109bfc106d503779f54f61433c7ad4f2c05
Piyush Paul <piyushpaul605@gmail.com>
2024-11-10 01:01:06 +0530

    fixed payment-success datafetching

--- src/app/checkout/payment-success/page.tsx
+++ src/app/checkout/payment-success/page.tsx
@@ -1,23 +1,35 @@
 'use client';
-import { useEffect } from 'react';
-import { redirect, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
 import { Button } from '@/components/ui/button';
+
+interface ResponseData {
+  price: string;
+  planType: {
+    desc: string;
+    productName: string;
+  };
+  invoiceURL: string;
+}
 
 export default function PaymentSuccess() {
   const searchParams = useSearchParams();
   const session_id = searchParams.get('session_id');
+  const [responseData, setResponseData] = useState<ResponseData>();
 
   useEffect(() => {
     async function verifyPayment() {
         if (session_id) {
             const response = await fetch('/api/subscription/update-subscription', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ sessionId: session_id }),
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({ sessionId: session_id }),
             });
-
+            const res = await response.json();
+            // console.log(JSON.stringify(res.respData));
+            setResponseData(res.respData);
             if (!response.ok) {
-            console.error('Failed to update subscription');
+              console.error('Failed to update subscription');
             }
         }
     }
@@ -25,9 +37,15 @@ export default function PaymentSuccess() {
     verifyPayment();
   }, [session_id]);
 
+  if (!responseData) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <div>
         <h1>Payment Successful! Your subscription is now active.</h1>
+        <h1>{responseData.price}</h1>
+        <h2>{responseData.planType.productName}</h2>
         <Button><a href={`${process.env.NEXT_PUBLIC_API_BASE_URL}/dashboard/home`}>Go to dashboard</a></Button>
     </div>
   );
