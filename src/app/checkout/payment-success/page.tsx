0d2681a3a99fbd76edf1da9106dddb63c0d3e16b
shibammitra24 <shibammitra695@gmail.com>
2024-11-10 00:07:44 +0530

    Developed checkout page

--- src/app/checkout/payment-success/page.tsx
+++ src/app/checkout/payment-success/page.tsx
@@ -1,25 +1,25 @@
-'use client';
-import { useEffect } from 'react';
-import { redirect, useSearchParams } from 'next/navigation';
-import { Button } from '@/components/ui/button';
+"use client";
+import { useEffect } from "react";
+import { redirect, useSearchParams } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export default function PaymentSuccess() {
   const searchParams = useSearchParams();
-  const session_id = searchParams.get('session_id');
+  const session_id = searchParams.get("session_id");
 
   useEffect(() => {
     async function verifyPayment() {
-        if (session_id) {
-            const response = await fetch('/api/subscription/update-subscription', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ sessionId: session_id }),
-            });
+      if (session_id) {
+        const response = await fetch("/api/subscription/update-subscription", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ sessionId: session_id }),
+        });
 
-            if (!response.ok) {
-            console.error('Failed to update subscription');
-            }
+        if (!response.ok) {
+          console.error("Failed to update subscription");
         }
+      }
     }
 
     verifyPayment();
@@ -27,8 +27,12 @@ export default function PaymentSuccess() {
 
   return (
     <div>
-        <h1>Payment Successful! Your subscription is now active.</h1>
-        <Button><a href={`${process.env.NEXT_PUBLIC_API_BASE_URL}/dashboard/home`}>Go to dashboard</a></Button>
+      <h1>Payment Successful! Your subscription is now active.</h1>
+      <Button>
+        <a href={`${process.env.NEXT_PUBLIC_API_BASE_URL}/dashboard/home`}>
+          Go to dashboard
+        </a>
+      </Button>
     </div>
   );
 }
