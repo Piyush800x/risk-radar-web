37b9dcf6b41f323d94b2760f84e9bd2f22b1e792
shibammitra24 <shibammitra695@gmail.com>
2024-11-16 19:28:09 +0530

    Added Loading animations in Dashboard and Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,52 @@
-'use client';
+"use client";
 import { useEffect, useState } from "react";
 import { LandingPage } from "@/components/LandingPage/LandingPage";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
-
+import { Loader } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
   const [loading, setLoading] = useState<boolean>(true);
-  const {user, isAuthenticated} = useKindeBrowserClient();
+  const { user, isAuthenticated } = useKindeBrowserClient();
 
   const checkuser = async () => {
     setLoading(true);
     const userData = {
-      authId: user?.id, 
-      authEmailId: user?.email, 
+      authId: user?.id,
+      authEmailId: user?.email,
       userFirstName: user?.given_name,
       userLastName: user?.family_name,
-    }
+    };
 
     try {
-      const req = await fetch('/api/check-user', {
+      const req = await fetch("/api/check-user", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(userData)
+        body: JSON.stringify(userData),
       });
 
-      await req.json(); 
-    }
-    catch (error) {
+      await req.json();
+    } catch (error) {
       console.error(error);
-    }
-    finally {
+    } finally {
       setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-    checkuser()
+    checkuser();
   }, [isAuthenticated]);
 
   if (loading) {
-    return <div>Loading...</div>
+    return (
+      <div className="flex flex-col w-full h-screen items-center justify-center">
+        <Loader className="animate-spin size-10" />
+      </div>
+    );
   }
-  
+
   return (
     <div>
       <LandingPage />
--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as React from "react";
+import { useState, useEffect } from "react";
 import LandingNavBar from "./sections/LandingNavBar";
 import Hero from "./sections/Hero";
 import { useTheme } from "next-themes";
@@ -9,14 +9,26 @@ import Trust from "./sections/Trust";
 import LandingPricing from "./sections/LandingPricing";
 import Footer from "./sections/Footer";
 import Faq from "./sections/Faq";
+import { Button } from "../ui/button";
+import { ChevronUp } from "lucide-react";
 
 export function LandingPage() {
   const { setTheme } = useTheme();
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsVisible(window.scrollY > 300);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   setTheme("dark");
 
   return (
-    <div className="flex flex-col min-h-[100dvh]">
+    <div className="flex flex-col min-h-[100dvh] select-none">
       <div>
         {/* ALl sections div without Footer */}
         <div className="sm:mx-40 mx-4">
@@ -36,6 +48,15 @@ export function LandingPage() {
         </div>
         <Footer />
       </div>
+
+      {isVisible && (
+        <Button
+          className="fixed w-10 h-10 bottom-5 right-5 bg-white/20 text-white p-3 rounded-full shadow-lg hover:bg-white/40 transition duration-300"
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        >
+          <ChevronUp />
+        </Button>
+      )}
     </div>
   );
 }
